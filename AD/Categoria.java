523f69ebd5537f225b847491f64f7fb81cb4fe7b
Monica Blanco <114340444+Monica-74@users.noreply.github.com>
2024-11-21 14:13:38 +0100

    Categoria.java
    21/11

--- AD/Categoria.java
+++ AD/Categoria.java
@@ -1,21 +1,36 @@
 package org.example;
+import javax.annotation.processing.Generated;
 import  javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.GeneratedValue; //indica que el valor de la clave primaria es generada automáticamente por la BBDD.
+import javax.persistence.GenerationType;
+import javax.persistence.Id; //para especificar cual será la clave primaria de la entidad.
+import javax.persistence.JoinColumn; //sirve para especificar que esta columna se usará para unir dos tablas en la relación.
+import javax.persistence.ManyToOne; //define una entidad de muchos a uno o lo que es lo mismo N:1
 
 @Entity
+
 @Table(name ="Categoria")
 
-
 public class Categoria {
+    @Id  //con ello estoy definiendo que tengo una clave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //esta anotación me hace falta para que hibernate incremente automáticamente los valores. Sustituye
+    //al increment en la BBDD cuando lo generamos. Si mi clave primaria fuera nombre no haría falta poner este código.
+    private int idCategoria;
+
     private String alimentosUltracongelados;
     private  String alimentosCongelados;
     private String alimentosRefrigerados;
     private String alimentosFrescos;
-    private int idProveedor;
-    private int idProducto;
-    private int idCategoria;
+    @ManyToOne //relacion N:1 muchos a uno
+    @JoinColumn(name = "idProveedor", referencedColumnName = "idProveedor")
+    private Proveedor proveedor; // esta es la clave foránea de proveedor
+
+    @ManyToOne
+    @JoinColumn(name = "idProducto", referencedColumnName = "idProducto") //se utiliza esta columna como clave foranea para referenciar la clave primaria
+    private Producto producto; // instanciamos la clase Producto con el objeto producto.
 
     private List<Categoria>Categorias = new ArrayList<>();
 
@@ -26,15 +41,6 @@ public class Categoria {
     public String getAlimentosUltracongelados() {
         return alimentosUltracongelados;
     }
-
-    public int getIdProveedor() {
-        return idProveedor;
-    }
-
-    public int getIdProducto() {
-        return idProducto;
-    }
-
     public String getAlimentosCongelados() {
         return alimentosCongelados;
     }
@@ -49,6 +55,13 @@ public class Categoria {
 
     public List<Categoria> getCategorias() {
         return Categorias;
+    }
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public Producto getProducto() {
+        return producto;
     }
 
     public void setIdCategoria(int idCategoria) {
@@ -75,13 +88,11 @@ public class Categoria {
         this.Categorias = Categorias;
     }
 
-    public void setIdProveedor(int idProveedor) {
-        this.idProveedor = idProveedor;
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
     }
 
-    public void setIdProducto(int idProducto) {
-        this.idProducto = idProducto;
+    public void setProducto(Producto producto) {
+        this.producto = producto;
     }
-
-
 }
